6eaa79fb7295d6201416dace58abd83329d27854
Thanga <thangamariappancse@gmail.com>
2022-02-02 11:32:38 +0530

    updates

--- Firebase/Firebase.js
+++ Firebase/Firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyDrqT5HBScp0v2_N43f5CaBR0ObqNZe9AI",
+  authDomain: "techsheet-d41c2.firebaseapp.com",
+  projectId: "techsheet-d41c2",
+  storageBucket: "techsheet-d41c2.appspot.com",
+  messagingSenderId: "290346011418",
+  appId: "1:290346011418:web:d14e8600dbdc3cbb6ffaa3",
+  measurementId: "G-RG39JLNYG7"
 };
 const app = initializeApp(firebaseConfig);
 
